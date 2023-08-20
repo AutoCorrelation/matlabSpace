944c52fc376dce11e66cb4d69c7968e64c8f5933
Chung-MinJun <126836852+Chung-MinJun@users.noreply.github.com>
2023-08-21 02:08:22 +0900

    0821

--- KF.m
+++ KF.m
@@ -1,22 +1,58 @@
-function [output,err] = KF(Npoints,initial_state,true_state,measurement,position_noise)
-output=zeros(size(initial_state, 1), Npoints);
-q_output=zeros(1, Npoints);
-A = [1 10/Npoints;0 1];
-H = [1 0; 0 1];
-R = [position_noise^2 0; 0 0.1^2];
-Q = eye(size(initial_state,1));
-x_hat_k1=initial_state;
-P_k1 = zeros(size(initial_state,1));
+function [est,error_est,error_theory] = KF(Npoints,initial_state,true_state,measurement,position_variance)
+%estimate KF
+A=[1 10/Npoints; 0 1];
+H=[1 0;0 1];
+R=[position_variance^2 0;0 0.01];
+Q=zeros(2,2);  %size(initial_state,1)
 
-for t = 1:Npoints
-    predict_x_hat_k = A*x_hat_k1;
-    predict_P_k = A*P_k1*A' + Q;
-    K = predict_P_k*H'/(H*predict_P_k*H'+R);
-    x_hat_k1=predict_x_hat_k+K*(measurement(:,t)-H*predict_x_hat_k);
-    output(:,t) = x_hat_k1;
-    P_k1=predict_P_k-K*H*predict_P_k;
-    Q=(x_hat_k1-predict_x_hat_k)*(x_hat_k1-predict_x_hat_k)'; % x_hat_k1=estimate, true_state=Theory
-    q_output(1,t)=x_hat_k1(1,1)-predict_x_hat_k(1,1);
+est=zeros(2,Npoints);
+%theory=zeros(2,1);
+%step 1
+state=initial_state;
+error_covariance=zeros(2,2);
+
+for t=1:Npoints
+    %step 2
+    predict_state=A*state;
+    predict_error_covariance=A*error_covariance*A'+Q;
+    %get error
+    err(t,1)=abs(state(1,1)-measurement(1,t));
+    %step 3
+    K=predict_error_covariance*H'/(H*predict_error_covariance*H'+R);
+    %step 4
+    state=predict_state+K*(measurement(:,t)-H*predict_state);
+    est(:,t)=state;
+    %step 5
+    error_covariance=(1-K*H)*predict_error_covariance;
+    Q=(predict_state-state)*(predict_state-state)';
 end
-err=abs(sum(q_output,'all')/Npoints);
+error_est=sum(err(:,1),'all')/Npoints;
 
+% theory KF
+A=[1 10/Npoints; 0 1];
+H=[1 0;0 1];
+R=[position_variance^2 0;0 0.01];
+Q=zeros(2,2);  %size(initial_state,1)
+err=zeros(Npoints,1);
+est=zeros(2,Npoints);
+%theory=zeros(2,Npoints);
+%step 1
+state=initial_state;
+error_covariance=zeros(2,2);
+
+for t=1:Npoints
+    %step 2
+    predict_state=A*state;
+    predict_error_covariance=A*error_covariance*A'+Q;
+    %get error
+    err(t,1)=abs(state(1,1)-measurement(1,t));
+    %step 3
+    K=predict_error_covariance*H'/(H*predict_error_covariance*H'+R);
+    %step 4
+    state=predict_state+K*(measurement(:,t)-H*predict_state);
+    est(:,t)=state;
+    %step 5
+    error_covariance=(1-K*H)*predict_error_covariance;
+    Q=(measurement(:,t)-predict_state)*(measurement(:,t)-predict_state)';
+end
+error_theory=sum(err(:,1),'all')/Npoints;
--- KFMAIN.m
+++ KFMAIN.m
@@ -5,14 +5,14 @@ close all;
 position_variance=[0.001,0.01,0.1,1,10,100];
 velocity_variance=0.1;
 
-iter=1;
+iter=5;
 
 a=0.1*pi;
 Npoints = 10;
 t = linspace(0, 10, Npoints);
 true_state = [sin(a*t);a*cos(a*t)];
 %plot(t,true_position);
-initial_state=[0; 0];
+initial_state=[0; a*cos(0)];
 
 for i=1:iter
     for j=1:Npoints
@@ -23,14 +23,15 @@ for i=1:iter
         measurement10=true_state+[position_variance(1,5)*randn(1,Npoints);velocity_variance*randn(1,Npoints)];
         measurement100=true_state+[position_variance(1,6)*randn(1,Npoints);velocity_variance*randn(1,Npoints)];
         
-        [estimated_state0001,err0001]=KF(Npoints,initial_state,true_state,measurement0001,0.001);
-        [estimated_state001,err001]=KF(Npoints,initial_state,true_state,measurement001,0.01);
-        [estimated_state01,err01]=KF(Npoints,initial_state,true_state,measurement01,0.1);
-        [estimated_state1,err1]=KF(Npoints,initial_state,true_state,measurement1,1);
-        [estimated_state10,err10]=KF(Npoints,initial_state,true_state,measurement10,10);
-        [estimated_state100,err100]=KF(Npoints,initial_state,true_state,measurement100,100);
-        err=[err0001 err001 err01 err1 err10 err100];
-
+        [estimated_state0001,error_est0001,error_theory0001]=KF(Npoints,initial_state,true_state,measurement0001,0.001);
+        [estimated_state001,error_est001,error_theory001]=KF(Npoints,initial_state,true_state,measurement001,0.01);
+        [estimated_state01,error_est01,error_theory01]=KF(Npoints,initial_state,true_state,measurement01,0.1);
+        [estimated_state1,error_est1,error_theory1]=KF(Npoints,initial_state,true_state,measurement1,1);
+        [estimated_state10,error_est10,error_theory10]=KF(Npoints,initial_state,true_state,measurement10,10);
+        [estimated_state100,error_est100,error_theory100]=KF(Npoints,initial_state,true_state,measurement100,100);
+        err_est{i}=[error_est0001, error_est001, error_est01, error_est1, error_est10, error_est100];
+        err_theory{i}=[error_theory0001 error_theory001 error_theory01 error_theory1 error_theory10 error_theory100];
+        
         all_estimated_state0001{i}=estimated_state0001;
         all_estimated_state001{i}=estimated_state001;
         all_estimated_state01{i}=estimated_state01;
@@ -43,11 +44,20 @@ end
 figure;
 hold on;
 plot(t, true_state(1, :), 'g', 'LineWidth', 2);
-plot(t, measurement001(1, :), 'b.', 'MarkerSize', 10);
-plot(t, all_estimated_state001{1}(1, :), 'r', 'LineWidth', 1);
+plot(t, measurement1(1, :), 'b.', 'MarkerSize', 10);
+plot(t, all_estimated_state1{2}(1, :), 'r', 'LineWidth', 1);
 xlabel('Time');
 ylabel('Position');
 legend('True Position', 'Measurements', 'Estimated Position');
 title('Position Estimation using Kalman Filter');
+hold off
 figure;
-semilogx(position_variance,err(1,:));
+semilogx(position_variance,err_est{:,1}(1,:),"b");
+grid on
+hold on
+semilogx(position_variance,err_theory{:,1}(1,:),"r");
+xlabel('Time');
+ylabel('err');
+legend('est', 'theory');
+title('comparing err each variance using Kalman Filter');
+
