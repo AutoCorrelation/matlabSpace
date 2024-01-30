0c9ffde6975e3d2828d01fd024b9853849c6e81b
Chung-MinJun <126836852+Chung-MinJun@users.noreply.github.com>
2024-01-30 15:21:13 +0900

    Add files via upload

--- Measurement_Data.m
+++ Measurement_Data.m
@@ -12,16 +12,16 @@ fileID4_0001 = fopen('Measurement_from_Anchor4_at_MNV_0001.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_0001(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_0001(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_0001(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_0001(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
     end    
-    fprintf(fileID1_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_0001);
-    fprintf(fileID2_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_0001);
-    fprintf(fileID3_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_0001);
-    fprintf(fileID4_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_0001);    
+    fprintf(fileID1_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_0001);
+    fprintf(fileID2_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_0001);
+    fprintf(fileID3_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_0001);
+    fprintf(fileID4_0001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_0001);    
 end
 fclose(fileID1_0001);
 fclose(fileID2_0001);
@@ -40,16 +40,16 @@ fileID4_001 = fopen('Measurement_from_Anchor4_at_MNV_001.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_001(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_001(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_001(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_001(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
     end
-    fprintf(fileID1_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_001);
-    fprintf(fileID2_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_001);
-    fprintf(fileID3_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_001);
-    fprintf(fileID4_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_001);    
+    fprintf(fileID1_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_001);
+    fprintf(fileID2_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_001);
+    fprintf(fileID3_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_001);
+    fprintf(fileID4_001,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_001);    
 end
 fclose(fileID1_001);
 fclose(fileID2_001);
@@ -68,16 +68,16 @@ fileID4_01 = fopen('Measurement_from_Anchor4_at_MNV_01.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_01(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_01(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_01(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_01(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
     end
-    fprintf(fileID1_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_01);
-    fprintf(fileID2_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_01);
-    fprintf(fileID3_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_01);
-    fprintf(fileID4_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_01);    
+    fprintf(fileID1_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_01);
+    fprintf(fileID2_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_01);
+    fprintf(fileID3_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_01);
+    fprintf(fileID4_01,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_01);    
 end
 fclose(fileID1_01);
 fclose(fileID2_01);
@@ -96,16 +96,16 @@ fileID4_1 = fopen('Measurement_from_Anchor4_at_MNV_1.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_1(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_1(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_1(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_1(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);       
     end
-    fprintf(fileID1_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_1);
-    fprintf(fileID2_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_1);
-    fprintf(fileID3_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_1);
-    fprintf(fileID4_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_1);    
+    fprintf(fileID1_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_1);
+    fprintf(fileID2_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_1);
+    fprintf(fileID3_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_1);
+    fprintf(fileID4_1,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_1);    
 end
 fclose(fileID1_1);
 fclose(fileID2_1);
@@ -124,16 +124,16 @@ fileID4_10 = fopen('Measurement_from_Anchor4_at_MNV_10.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_10(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_10(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_10(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_10(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);    
     end
-    fprintf(fileID1_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_10);
-    fprintf(fileID2_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_10);
-    fprintf(fileID3_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_10);
-    fprintf(fileID4_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_10);    
+    fprintf(fileID1_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_10);
+    fprintf(fileID2_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_10);
+    fprintf(fileID3_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_10);
+    fprintf(fileID4_10,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_10);    
 end
 fclose(fileID1_10);
 fclose(fileID2_10);
@@ -152,16 +152,16 @@ fileID4_100 = fopen('Measurement_from_Anchor4_at_MNV_100.txt','w');
 
 for num1=1:iter    
     for num2=1:NPoints
-        UserPos = (num2-1)*[1 1];
+        UserPos = (num2)*[1 1];
         Measurement1_100(num2,1) = (sqrt(sum((Anchor1-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);        
         Measurement2_100(num2,1) = (sqrt(sum((Anchor2-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement3_100(num2,1) = (sqrt(sum((Anchor3-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
         Measurement4_100(num2,1) = (sqrt(sum((Anchor4-UserPos).^2))+ sqrt(Measurement_Noise_Variance)*randn);
     end
-    fprintf(fileID1_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_100);
-    fprintf(fileID2_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_100);
-    fprintf(fileID3_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_100);
-    fprintf(fileID4_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_100);    
+    fprintf(fileID1_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement1_100);
+    fprintf(fileID2_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement2_100);
+    fprintf(fileID3_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement3_100);
+    fprintf(fileID4_100,'%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f \n',Measurement4_100);    
 end
 fclose(fileID1_100);
 fclose(fileID2_100);
