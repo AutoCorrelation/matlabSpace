33aabc4ccca03488abf938fcf233ff336a97cf7d
AutoCorrelation <minjun.chung@gmail.com>
2024-06-18 11:21:13 +0900

    1semester complete

---
+++ Assignment/dataPlotting/Copy_2_of_experimentPlot.m
@@ -0,0 +1,34 @@
+clear
+close all
+clc
+format long
+
+hold on
+grid on
+% rc = [ 10/0.47 0;10/1.0 0;10/1.2 0;10/1.8 0;10/2.4 0;10/2.7 0];
+% Vcc = [0 10;0 10;0 10;0 10;0 10;0 10;];
+% %시뮬레이션 값
+% simuled_Icq = [0 3.639 4.084 4.602 4.733 4.779 4.903];
+% simuled_Vceq = [0 0.1760 0.1974 1.717 4.320 5.222 7.695];
+%실험 결과 값
+measured_Icq = [ 0
+    0
+    0
+    0
+    0.131
+    35.191
+    37.568
+    39.829
+    ];
+measured_Vceq = [0 0.2 0.6 1.0 1.4 1.6 1.8 2.0 ];
+% for num1 = 1:6
+%     p=plot(Vcc (num1,:),rc(num1,:),'b:');
+% end
+p.Color = '#00841a';
+% plot(simuled_Vceq,simuled_Icq,'squre-','LineWidth',1.3);
+plot(measured_Vceq,measured_Icq,'o-','LineWidth',1.3);
+legend('R=0.47ohm','R=1.0ohm','R=1.2ohm','R=1.8ohm',...
+    'R=2.4ohm','R=2.8ohm','simuled Data','measured Data');
+xlabel('V_C_E [V]');
+ylabel('I_C [mA]');
+
--- Assignment/dataPlotting/Copy_of_experimentPlot.m
+++ Assignment/dataPlotting/Copy_of_experimentPlot.m
@@ -5,30 +5,32 @@ format long
 
 hold on
 grid on
-% rc = [ 10/0.47 0;10/1.0 0;10/1.2 0;10/1.8 0;10/2.4 0;10/2.7 0];
-% Vcc = [0 10;0 10;0 10;0 10;0 10;0 10;];
-% %시뮬레이션 값
-% simuled_Icq = [0 3.639 4.084 4.602 4.733 4.779 4.903];
-% simuled_Vceq = [0 0.1760 0.1974 1.717 4.320 5.222 7.695];
-%실험 결과 값
-measured_Icq = [ 0
-    0
-    0
-    0
-    0.131
-    35.191
-    37.568
-    39.829
+RL=[ 1 1.5 2 2.4 3 3.6 4.3];
+sim_Av = [
+    
+    -38.06
+    -46.24
+    -51.15
+    -53.68
+    -57.02
+    -60.28
+    -62.30
     ];
-measured_Vceq = [0 0.2 0.6 1.0 1.4 1.6 1.8 2.0 ];
-% for num1 = 1:6
-%     p=plot(Vcc (num1,:),rc(num1,:),'b:');
-% end
-p.Color = '#00841a';
-% plot(simuled_Vceq,simuled_Icq,'squre-','LineWidth',1.3);
-plot(measured_Vceq,measured_Icq,'o-','LineWidth',1.3);
-legend('R=0.47ohm','R=1.0ohm','R=1.2ohm','R=1.8ohm',...
-    'R=2.4ohm','R=2.8ohm','simuled Data','measured Data');
-xlabel('V_C_E [V]');
-ylabel('I_C [mA]');
+cal_Av = [
+    
+    -41.9758
+    -50.371
+    -55.9677
+    -59.26
+    -62.9637
+    -65.7013
+    -68.1117
+    ];
+hold on
+grid on
+plot(RL,abs(sim_Av),'*-','LineWidth',1,'Color',"r");
+plot(RL,abs(cal_Av),'^-','LineWidth',1,'Color',"b");
+legend('sim','cal');
+xlabel('R_L [㏀]');
+ylabel('|A_V| [V/V]');
 
